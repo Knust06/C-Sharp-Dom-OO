34517fc002a7f2976fff0651a2faac12d2e3f66a
Knust06 <lucasknust0605@gmail.com>
2024-07-14 14:16:51 -0300

    Finalizando a parte 4 do curso. Criando interface para contrato de avaliação e a possibilidade de avaliar um album da banda.

--- ScreenSound/Modelos/Banda.cs
+++ ScreenSound/Modelos/Banda.cs
@@ -1,6 +1,6 @@
 ﻿namespace ScreenSound.Modelos;
 
-internal class Banda
+internal class Banda : IAvaliavel
 {
     private List<Album> albuns = new List<Album>();
     private List<Avaliacao> notas = new List<Avaliacao>();
@@ -9,7 +9,6 @@ internal class Banda
     {
         Nome = nome;
     }
-
     public string Nome { get; }
     public double Media
     {
